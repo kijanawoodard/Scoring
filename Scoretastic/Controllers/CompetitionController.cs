866845ffecc6cddf0634a4f92110c020d9a0d96b
Kijana Woodard <github@wyldeye.com>
2011-11-26 20:42:25 -0600

    progress so far

---
+++ Scoretastic/Controllers/CompetitionController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Scoretastic.Infrastructure.AutoMapper;
+
+namespace Scoretastic.Controllers
+{
+    public class CompetitionController : RavenController
+    {
+        public ActionResult Index()
+        {
+            var competitions = RavenSession.Query<Competition>()
+                                .OrderBy(x => x.Name);
+
+            var model = new CompetitionIndexViewModel(competitions);
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var model = new CompetitionCreateViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Create(CompetitionCreateViewModel.ViewInput input, FormCollection form)
+        {
+            if (!ModelState.IsValid)
+                return ReturnReHydratedView(Create, input);
+
+            var competion = input.MapTo<Competition>();
+            RavenSession.Store(competion);
+
+            return RedirectToAction("Index");
+        }
+
+        
+
+    }
+    //make general error show up on page
+    public interface IViewModel<T>
+    {
+        T Input { get; set; }
+    }
+
+    public class Competition
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    //create a bit of ceremony to test out some concepts that could be quite useful later
+    public class CompetitionCreateViewModel : IViewModel<CompetitionCreateViewModel.ViewInput>
+    {
+        public CompetitionCreateViewModel()
+        {
+            Input = new ViewInput();
+        }
+
+        public ViewInput Input { get; set; }
+
+        public class ViewInput
+        {
+            [Required]
+            public string Name { get; set; }
+
+            [Required]
+            [DataType(DataType.MultilineText)]
+            public string Description { get; set; }
+        }
+    }
+
+    public class CompetitionIndexViewModel
+    {
+        public CompetitionIndexViewModel(IEnumerable<Competition> competitions)
+        {
+            Competitions = competitions;
+        }
+
+        public IEnumerable<Competition> Competitions { get; set; }
+    }
+}
---
+++ Scoretastic/Controllers/RavenController.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Web.Mvc;
+using Raven.Client;
+
+namespace Scoretastic.Controllers
+{
+    //https://github.com/ayende/RaccoonBlog/blob/master/HibernatingRhinos.Loci.Common/Controllers/RavenController.cs
+    public abstract class RavenController : Controller
+    {
+        public static IDocumentStore DocumentStore
+        {
+            get { return _documentStore; }
+            set
+            {
+                if (_documentStore == null)
+                {
+                    _documentStore = value;
+                }
+            }
+        }
+        private static IDocumentStore _documentStore;
+
+        public IDocumentSession RavenSession { get; protected set; }
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            RavenSession = _documentStore.OpenSession();
+        }
+
+        // TODO: Consider re-applying https://github.com/ayende/RaccoonBlog/commit/ff954e563e6996d44eb59a28f0abb2d3d9305ffe
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.IsChildAction)
+                return;
+
+            CompleteSessionHandler(filterContext);
+        }
+
+        protected ActionResult ReturnReHydratedView<T>(Func<ActionResult> action, T input) where T : class
+        {
+            var result = action();
+            var view = result as ViewResult;
+            if (view != null)
+            {
+                var model = view.Model as IViewModel<T>;
+                if (model != null)
+                    model.Input = input;
+            }
+
+            return result;
+        }
+
+        protected void CompleteSessionHandler(ActionExecutedContext filterContext)
+        {
+            using (RavenSession)
+            {
+                if (filterContext.Exception != null)
+                    return;
+
+                if (RavenSession != null)
+                    RavenSession.SaveChanges();
+            }
+
+//            TaskExecutor.StartExecuting();
+        }
+
+        protected HttpStatusCodeResult HttpNotModified()
+        {
+            return new HttpStatusCodeResult(304);
+        }
+
+//        protected ActionResult Xml(XDocument xml, string etag)
+//        {
+//            return new XmlResult(xml, etag);
+//        }
+    }
+}
